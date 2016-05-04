f0b32a6681b7a703ffd0747e7a17ff9700856917
PeeTee12 <petr.tobi@gmail.com>
2016-05-04 21:03:45 +0200

    Změna v AntHeartRate na tvorbu HDF5.

---
+++ src/cz/zcu/AntPlus2NIXConverter/Profiles/AntHeartRate.java
@@ -0,0 +1,70 @@
+package cz.zcu.AntPlus2NIXConverter.Profiles;
+
+import java.time.LocalDate;
+
+import com.dsi.ant.plugins.antplus.pcc.AntPlusHeartRatePcc;
+
+import cz.zcu.AntPlus2NIXConverter.Convert.ID;
+import cz.zcu.AntPlus2NIXConverter.Data.CreateFile;
+import cz.zcu.AntPlus2NIXConverter.Data.CreateDataArray;
+//import cz.zcu.AntPlus2NIXConverter.Data.Source;
+
+import org.g_node.nix.*;
+
+public class AntHeartRate {
+
+	private static int index = 0;
+	
+	//private Block block;
+	private DataArray dataHeartBeatCounter;
+	private DataArray dataComputedHeartRade;
+	private DataArray dataTimeOfPreviousHeartBeat;
+	private ID id;
+	private File file;
+
+	private AntPlusHeartRatePcc hr = null;
+
+	public AntHeartRate(int[] hearBeatCounter, int computedHeartRade, double[] timeOfPreviousHeartBeat) {
+
+		id = new ID();
+		/*createFile();
+		createBlock();
+		createSource();
+		createDataArrayHeartBeat(hearBeatCounter);
+		createDataArrayComputedHeartRate(computedHeartRade);
+		createDataArrayTimeOfPrevious(timeOfPreviousHeartBeat);*/
+		index++;
+	}
+
+	public AntHeartRate(AntPlusHeartRatePcc hr, int[] hearBeatCounter, int computedHeartRade,
+			double[] timeOfPreviousHeartBeat) {
+
+		this(hearBeatCounter, computedHeartRade, timeOfPreviousHeartBeat);
+		this.hr = hr;
+
+	}
+	
+	public void createFile(int[] heartBeatCounter, int[] computedHeartRate, double[] timeOfPreviousHeartBeat){
+		file = File.open("AntHeartRate.h5", FileMode.Overwrite);
+		file.setCreatedAt();
+		
+		Block block = file.createBlock("recording" + index, "recording");
+
+		Source source = block.createSource("HeartRate" + index, "antMessage");
+		
+		dataHeartBeatCounter = block.createDataArray("heartBeatCounter" + index, "antMessage", DataType.Int32,
+				null);
+			dataHeartBeatCounter.setData(heartBeatCounter, null, null);
+			
+		dataComputedHeartRade = block.createDataArray("computedHeartRate" + index, "antMessage", DataType.Int32,
+				null);
+			dataHeartBeatCounter.setData(computedHeartRate, null, null);
+
+		dataTimeOfPreviousHeartBeat = block.createDataArray("timeOfPreviousHeartBeat" + index, "antMessage", DataType.Double, 
+				null);
+			dataHeartBeatCounter.setData(timeOfPreviousHeartBeat, null, null);
+
+		file.close();
+	}
+
+}
