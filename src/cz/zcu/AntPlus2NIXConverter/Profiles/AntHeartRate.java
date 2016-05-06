26adef61790c56905dd8747f439ffd538151c91b
VenaJanoch <vena.janoch@windowslive.com>
2016-05-06 14:11:02 +0200

    Metoda pro ulozeni souboru nix

--- src/cz/zcu/AntPlus2NIXConverter/Profiles/AntHeartRate.java
+++ src/cz/zcu/AntPlus2NIXConverter/Profiles/AntHeartRate.java
@@ -1,8 +1,8 @@
 package cz.zcu.AntPlus2NIXConverter.Profiles;
 
-import com.dsi.ant.plugins.antplus.pcc.AntPlusHeartRatePcc;
 
 import cz.zcu.AntPlus2NIXConverter.Convert.ID;
+import cz.zcu.AntPlus2NIXConverter.Data.OdMLData;
 
 import org.g_node.nix.*;
 
@@ -17,43 +17,26 @@ public class AntHeartRate {
 	private DataArray dataComputedHeartRate;
 	private DataArray dataTimeOfPreviousHeartBeat;
 	private ID id;
-	private int[] pole = new int[2];
 
-	private AntPlusHeartRatePcc hr = null;
+	private int[] heartBeatCounter = new int[4];
+	private int[] computedHeartRate = new int[4];
+	private double[] timeOfPreviousHeartBeat = new double[3];
 
-	/*public AntHeartRate(int[] hearBeatCounter, int computedHeartRade, double[] timeOfPreviousHeartBeat) {
-
-		id = new ID();
-		/*createFile();
-		createBlock();
-		createSource();
-		createDataArrayHeartBeat(hearBeatCounter);
-		createDataArrayComputedHeartRate(computedHeartRade);
-		createDataArrayTimeOfPrevious(timeOfPreviousHeartBeat);*/
-		/*index++;
-	}
-
-	public AntHeartRate(AntPlusHeartRatePcc hr, int[] hearBeatCounter, int computedHeartRade,
-			double[] timeOfPreviousHeartBeat) {
-
-		this(hearBeatCounter, computedHeartRade, timeOfPreviousHeartBeat);
-		this.hr = hr;
-
-	}*/
+	public AntHeartRate(int[] hearBeatCounter, int[] computedHeartRate, double[] timeOfPreviousHeartBeat, OdMLData metaData) {
+		
+		this.heartBeatCounter = hearBeatCounter;
+		this.computedHeartRate = computedHeartRate;
+		this.timeOfPreviousHeartBeat = timeOfPreviousHeartBeat; 
 	
-	public void createFile(int[] heartBeatCounter, int[] computedHeartRate, double[] timeOfPreviousHeartBeat){
+	}
+	
+	public void createNixFile(){
 		file = File.open("AntHeartRateRate.h5", FileMode.Overwrite);
 		
 		block = file.createBlock("recording" + index, "recording");
 
 		source = block.createSource("HeartRate" + index, "antMessage");
-		
-		System.out.println("Block ID: " + block.getId());
-		System.out.println("Source ID: " + source.getId());
-		System.out.println("Created at: " + block.getCreatedAt());
-		
-		pole[0] = 1;
-		
+				
 		dataHeartBeatCounter = block.createDataArray("heartBeatCounter" + index, "antMessage", DataType.Int32,
 				new NDSize(new int[] {1,heartBeatCounter.length}));
 			dataHeartBeatCounter.setData(heartBeatCounter, new NDSize(new int [] {1,heartBeatCounter.length}), new NDSize(2,0));
@@ -69,9 +52,5 @@ public class AntHeartRate {
 		file.close();
 	}
 	
-	public static void main(String[] args) {
-		AntHeartRate a = new AntHeartRate();
-		a.createFile(new int [] {1,2,3,4,5,6,7,8,9}, new int[] {9,8,7,6,5,4,3,2,1}, new double[] {5.0,6.0,7.0,8.0,9.0,1.0,2.0,3.0});
-	}
-
+	
 }
