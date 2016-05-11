4f0b01e90aa19ece0d8beb99c5f720c7c124d69b
VenaJanoch <vena.janoch@windowslive.com>
2016-05-11 15:04:11 +0200

    Vytvorene rozhrani INixStream

--- src/cz/zcu/AntPlus2NIXConverter/Interface/IAntDat.java
+++
@@ -1,5 +0,0 @@
-package cz.zcu.AntPlus2NIXConverter.Interface;
-
-public interface IAntDat {
-
-}
---
+++ src/cz/zcu/AntPlus2NIXConverter/Interface/INixStream.java
@@ -0,0 +1,10 @@
+package cz.zcu.AntPlus2NIXConverter.Interface;
+
+import java.util.stream.Stream;
+
+import org.g_node.nix.File;
+
+public interface INixStream {
+
+	Stream<File> createNixFile();
+}
